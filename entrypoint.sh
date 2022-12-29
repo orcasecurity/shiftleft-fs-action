30870b4f76e0cf84f663b04cfe595cacebdfa52c
Guy Paz <guy.paz@orca.security>
2022-12-29 15:59:13 +0200

    fixing json

--- entrypoint.sh
+++ entrypoint.sh
@@ -121,7 +121,7 @@ annotate() {
     exit "${ORCA_EXIT_CODE}"
   fi
   mkdir -p "/app/${OUTPUT_FOR_JSON}"
-  cp "${OUTPUT_FOR_JSON}/fs.json" "/app/${OUTPUT_FOR_JSON}/"
+  cp "${OUTPUT_FOR_JSON}/file_system.json" "/app/${OUTPUT_FOR_JSON}/"
   cd /app || exit_with_err "error during annotations initiation"
   npm run build --if-present
   node dist/index.js
--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ function setWorkflowStatus(statusCode) {
 
 function main() {
   console.log("Processing Orca Shift-Left scan results...");
-  const jsonOutput = filepath.join(process.env.OUTPUT_FOR_JSON, "fs.json");
+  const jsonOutput = filepath.join(process.env.OUTPUT_FOR_JSON, "file_system.json");
   try {
     const parsedResults = readJSON(jsonOutput);
     annotator.annotateChangesWithResults(parsedResults);
